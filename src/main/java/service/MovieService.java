adda04487b1ba9fffdf7ebb36ef0e7b12952204a
IT24101981 <malshannandanayaka@gmail.com>
2025-05-17 11:43:20 +0530

    added update and delete movie function to movie service

--- src/main/java/service/MovieService.java
+++ src/main/java/service/MovieService.java
@@ -118,7 +118,7 @@ public class MovieService {
                 .max()
                 .orElse(0L) + 1;
     }
-   
+
     public Movie getMovieById(Long id) throws IOException {
         List<Movie> movies = getAllMovies();
         for (Movie movie : movies){
@@ -127,5 +127,28 @@ public class MovieService {
             }
         }
         return null;
+    }
+
+    //update and delete part(azib)
+
+    public void updateMovie(Movie updatedMovie) throws IOException {
+        synchronized (lock) {
+            List<Movie> movies = getAllMovies();
+            for (int i = 0; i < movies.size(); i++) {
+                if (movies.get(i).getId().equals(updatedMovie.getId())) {
+                    movies.set(i, updatedMovie);
+                    break;
+                }
+            }
+            saveAllMovies(movies);
+        }
+    }
+
+    public void deleteMovie(Long movieId) throws IOException {
+        synchronized (lock){
+            List<Movie> movies = getAllMovies();
+            movies.removeIf(movie -> movie.getId().equals(movieId));
+            saveAllMovies(movies);
+        }
     }
 }
