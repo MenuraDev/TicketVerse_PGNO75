a85f4760b0b5b29ff3790efee52515614f12240e
safran6ix <it24102097@my.sliit.lk>
2025-05-18 11:09:26 +0530

    User Management user model updated
    Signed-off-by: safran6ix <it24102097@my.sliit.lk>

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -82,4 +82,27 @@ public class User {
         this.firstName = firstName;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
